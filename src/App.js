f502dafb6b6c300876083876d1d027307d892eef
KolluriMounish <kollurimounish.com>
2022-12-04 03:10:51 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
       <h2>Hifi Apps</h2>
     </div>
     
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header-wraper">
+      <div className="main-info">
+        <canvas></canvas>
+        <h1>web development and websites promotions</h1>
+        <a href="#contact" className="btn-main-offer">contact me</a>
+      </div>
+    </div>
   )
 }
 
